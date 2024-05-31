96f16c63db844f4de4ac29df012d7e60eb2d5a32
Prasad Mistry <pmistry21@myseneca.ca>
2024-05-31 16:38:47 -0400

    Completed lab

--- lab3d.py
+++ lab3d.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-
 # Author ID: pmistry21@myseneca.ca
 
 import os
 
-# Finds free space.
 def free_space():
+    # Launches the Linux command: df -h | grep '/$' | awk '{print $4}' using os.system().
     command = "df -h | grep '/$' | awk '{print $4}'"
     result = os.popen(command).read().strip()
-    return result
+    # Returns a string in utf-8 format with newline characters stripped.
+    return result.encode('utf-8').decode('utf-8').strip('\n')
 
-# Prints the free space.
 if __name__ == "__main__":
+    # Prints the free disk space.
     print(free_space())
